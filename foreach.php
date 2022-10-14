382c35fc05a4387ebb8c614691ed43e73bdafdae
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-13 21:53:53 -0300

    Adicionando dados.

--- foreach.php
+++ foreach.php
@@ -1,18 +1,32 @@
 <?php
 
 $contasCorrentes = [
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Vinicius',
         'saldo' => 1000
     ],
-    12345678911 => [
+    '123.456.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    12325678910 => [
+    '123.256.789-10' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ]
+];
+
+// // Acréscimo de conta corrente na posição '123.258.852-12' do array associativo:
+// $contasCorrentes['123.258.852-12'] = [
+//     'titular' => 'Claudia',
+//     'saldo' => 2000
+// ];
+
+// Acréscimo de conta corrente na última posição do array associativo:
+$contasCorrentes[] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
+    // A chave do array associativo neste objeto será o maior inteiro 
+    // disponível no array, no caso zero.
 ];
 
 foreach ($contasCorrentes as $conta) {
--- listas.php
+++ listas.php
@@ -5,4 +5,10 @@ $idadeList = array(21, 23, 19, 25, 30, 41, 18); // Sintaxe original para array.
 
 $umaIdade = $idadeList[4]; // Busca pelo quinto item do array.
 
-echo $umaIdade;
+// $idadeList[7] = 20; // Acréscimo da idade 20 na posição 8 do array indexado.
+$idadeList[count($idadeList)] = 20; // Acréscimo da idade 20 na última posição do array indexado.
+// $idadeList[] = 20; // Também acrescenta a idade 20 na última posição do array indexado.
+
+foreach($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
