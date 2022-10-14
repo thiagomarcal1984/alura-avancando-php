76b08ee632d92f0a7d217ac725f0a77fe1425d16
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-13 21:20:04 -0300

    Lista de dados.

---
+++ listas.php
@@ -0,0 +1,8 @@
+<?php
+
+$idadeList = array(21, 23, 19, 25, 30, 41, 18); // Sintaxe original para array.
+// $idadeList = [21, 23, 19, 25, 30, 41, 18]; // Sintaxe introduzida no PHP 5.4.
+
+$umaIdade = $idadeList[4]; // Busca pelo quinto item do array.
+
+echo $umaIdade;
